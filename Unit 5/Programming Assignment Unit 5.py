7bd997b122ee85fa53df938d8a5bd5263b15ac2b
Andrew LaNoue Personal <alanoue@catalyte.io>
2023-07-19 23:21:22 -0400

    Fixed typo and bug

--- Unit 5/Programming Assignment Unit 5.py
+++ Unit 5/Programming Assignment Unit 5.py
@@ -11,32 +11,8 @@ def my_sqrt(a):
 
 
 def test_sqrt(num):
-     _my_sqrt = my_sqrt(num)
-     _sqrt = math.sqrt(num)
-     print(f"num = {num} | my_sqrt(num) = {_my_sqrt} | math.sqrt(num) = {_sqrt} | diff = {abs(_my_sqrt - _sqrt)}")
+     while num > 0: 
+          print(f"num = {num} | my_sqrt(num) = {my_sqrt(num)} | math.sqrt(num) = {math.sqrt(num)} | diff = {abs(my_sqrt(num) - math.sqrt(num))}")
+          num -= 1
      
-test_sqrt(1)
-test_sqrt(2)
-test_sqrt(3)
-test_sqrt(4)
-test_sqrt(5)
-test_sqrt(6)
-test_sqrt(7)
-test_sqrt(8)
-test_sqrt(9)
-test_sqrt(10)
-test_sqrt(11)
-test_sqrt(12)
-test_sqrt(13)
-test_sqrt(14)
-test_sqrt(15)
-test_sqrt(16)
-test_sqrt(17)
-test_sqrt(18)
-test_sqrt(19)
-test_sqrt(20)
-test_sqrt(21)
-test_sqrt(22)
-test_sqrt(23)
-test_sqrt(24)
 test_sqrt(25)
