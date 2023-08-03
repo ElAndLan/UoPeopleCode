8129ab36509aa939d31d87b36737c3bb0bc83351
Andrew LaNoue Personal <alanoue@catalyte.io>
2023-08-02 23:05:34 -0400

    Committing and pushing for Programming Assign Unit 7

--- Unit 7/Programming Assignment Unit 7.py
+++ Unit 7/Programming Assignment Unit 7.py
@@ -1,6 +1,6 @@
 alphabet = "abcdefghijklmnopqrstuvwxyz"   
 
-test_dups = ["zzz","dog","bookkeeper","subdermatoglyphic","subdermatoglyphics"] 
+test_dups = ["zzz","dog","bookkeeper","subdermatoglyphic","subdermatoglyphics", ] 
 
 test_miss = ["zzz","subdermatoglyphic","the quick brown fox jumps over the lazy dog"] 
 
@@ -12,18 +12,36 @@ def histogram(s):
           else:
                d[c] += 1
      return d 
+
  
 def has_duplicates(s):
-    _dict = dict()
+    _histogram = histogram(s)
     
-    for char in s:
-        if char in _dict:
-            print(char)
+    for key, value in _histogram.items():
+        if value > 1: 
             return True
-        _dict[char] = True
 
     return False
 
 
 
-print(has_duplicates(test_dups))
+for item in test_dups:
+    if has_duplicates(item):
+        print(item, 'has duplicates')
+    else: print(item, 'has no duplicates detected.')
+    
+def missing_letters(s):
+    _histogram = histogram(s)
+    _missing = []
+    
+    for char in alphabet:
+        if char not in _histogram:
+            _missing.append(char)
+            
+    return ''.join(_missing)
+
+for item in test_miss:
+    _missing = missing_letters(item)
+    if len(_missing):
+        print (item, 'is missing letters', _missing)
+    else: print(item, 'uses all the letters.')
